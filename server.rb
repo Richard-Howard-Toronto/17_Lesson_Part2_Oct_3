0bce68e819675e117562e521d6f84241f0928882
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-03 17:05:54 -0400

    505

--- server.rb
+++ server.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 
 get '/home' do
-  # instructions for what to do when user visits '/home' will go here
   erb :index
+end
+
+get '/gallery' do
+  erb :gallery
+end
+
+get '/about_me' do
+    @skills = ['git', 'HTML', 'CSS', 'Ruby']
+    @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb :about_me
 end
